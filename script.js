ac95588076760bfe95387077ce3edd98078c4d24
brijesh patel <pate0695@algonquinlive.com>
2020-01-10 12:09:53 -0500

    added changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let name = 'BRIJESH PATEL'
-let num = 2
-console.log('Hello my name is ${name}. ')
-console log('${num * 2 * 5 / num - 7}')
+let num = '2'
+console.log(`Hello my name is ${name}. `)
+console log(`${num * 2 * 5 / num - 7}`)
